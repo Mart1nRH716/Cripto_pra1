e687fa3b53bf149c3fbe2801855e03ded65738f4
Martin Romero <nahual332@outlook.com>
2024-02-28 11:47:15 -0600

    ya quedo, se modifico la funcion de multiplicacion y se quitaron varias funciones

--- multi_redux.py
+++ multi_redux.py
@@ -8,22 +8,35 @@ Output:
         P(x) = 0x11B = 283
 """
 
-#Multiplica dos numeros hexadecimales y devuelve el valor en hexadecimal
 def multiplicacion(hexa1, hexa2):
     # Convertir los números hexadecimales a enteros
     num1 = int(hexa1, 16)
     num2 = int(hexa2, 16)
-    # Multiplicar los números
-    resultado = num1 * num2
-    # Convertir el resultado de nuevo a hexadecimal
-    resultado_hex = hex(resultado)
+    
+    # Realizar la multiplicación en GF(2^8)
+    producto = 0
+    while num1 and num2:
+        if num2 & 1:
+            producto ^= num1
+        num2 >>= 1
+        num1 <<= 1
+        if num1 & 0x100:
+            num1 ^= 0x11B  # Polinomio irreducible x^8 + x^4 + x^3 + x + 1
+
+    # Si el producto es mayor que 255, aplicamos la reducción módulo P(x)
+    if producto > 255:
+        producto ^= 0x11B
+    
+    # Convertir el producto de nuevo a hexadecimal
+    resultado_hex = hex(producto)
     return resultado_hex
 
 #Psar de hexadecimal a polinomio
 #Devuelve un string que corresponde al hexadecimal
 def hex_polinomio(hex):
     lista = []
-    cadena_binaria = bin(int(hex,16)) #Cambiamos de hexadecimal a binario para identifcar los 1 y guardar las posiciones
+    cadena_binaria = bin(int(hex, 16))[2:] #Cambiamos de hexadecimal a binario para identifcar los 1 y guardar las posiciones
+    cadena_binaria = cadena_binaria[::-1]
     for i in range(len(cadena_binaria)):
         if cadena_binaria[i] == "1":
             lista.append(i)
@@ -36,44 +49,13 @@ def hex_polinomio(hex):
             polinomio += f"x^{i} +"
     return polinomio
 
-#Recibe dos numeros en hexadecimal
-#No se si devolver el resultado en hexadecimal
-def operacion_xor(hex1, hex2):
-    # Ejemplo 2
-    num1 = int(hex1, 16)
-    num2 = int(hex2, 16)
-    result = num1 ^ num2 #Operacion XOR entre numeros
-    return result
-
-#Recibe un numero hexadecimal y extra el sobrante
-def extraer_residuo(num_hex):
-    return str(num_hex)[0]
-
-def reducir_modulo(resultado_hex):
-    # Convertir el resultado a entero
-    resultado_entero = int(resultado_hex, 16)
-    # Definir el polinomio irreducible P(x) = x^8 + x^4 + x^3 + x + 1
-    polinomio_irreducible = 0x11B
-    # Realizar la reducción módulo el polinomio irreducible
-    while resultado_entero >= 256:
-        reduccion = polinomio_irreducible << (resultado_entero.bit_length() - 9)
-        resultado_entero ^= reduccion
-    # Convertir el resultado reducido de nuevo a hexadecimal
-    resultado_hex_reducido = hex(resultado_entero)
-    return resultado_hex_reducido
 
 
 # Ejemplo de uso:
 num1 = input("Ingrese el primer numero hexadecimal: ")
 num2 = input("Ingrese el segundo numero hexadecimal: ")
 
-resultado_multi = multiplicacion(num1, num2)
-print(f'El resulatdo de la multiplicacion es :  {resultado_multi}')
+resultado = multiplicacion(num1, num2)
+print(f'El resulatdo de la multiplicacion es :  {resultado}')
 
-print(f'El resultado de la multiplicacion (polinomio) :  {hex_polinomio(resultado_multi)}')
-
-print(reducir_modulo(resultado_multi))
-
-# dividendo = binario_polinomio("110110110110")
-# divisor = [8, 4, 3, 1, 0]  # Representa x^8 + x^4 + x^3 + x + 1
-# print("El residuo de la división es:")
+print(f'El resultado de la multiplicacion (polinomio) :  {hex_polinomio(resultado)}')
--- pruebas.py
+++ pruebas.py
@@ -1,18 +1,28 @@
-def hexadecimal_a_binario(hexadecimal):
-    # Convierte el número hexadecimal a un entero
-    entero = int(hexadecimal, 16)
-    # Convierte el entero a una cadena binaria y elimina el prefijo '0b'
-    binario = format(entero, 'b')
-    return binario
+def multiplicacion(hexa1, hexa2):
+    # Convertir los números hexadecimales a enteros
+    num1 = int(hexa1, 16)
+    num2 = int(hexa2, 16)
+    
+    # Realizar la multiplicación en GF(2^8)
+    producto = 0
+    while num1 and num2:
+        if num2 & 1:
+            producto ^= num1
+        num2 >>= 1
+        num1 <<= 1
+        if num1 & 0x100:
+            num1 ^= 0x11B  # Polinomio irreducible x^8 + x^4 + x^3 + x + 1
 
-def binario_polinomio(binario):
-    lista = []
-    cadena_binaria = str(binario)
-    for i in range(len(cadena_binaria)):
-        if cadena_binaria[i] == "1":
-            lista.append(i)
-    return lista
+    # Si el producto es mayor que 255, aplicamos la reducción módulo P(x)
+    if producto > 255:
+        producto ^= 0x11B
+    
+    # Convertir el producto de nuevo a hexadecimal
+    resultado_hex = hex(producto)
+    return resultado_hex
 
-
-var = binario_polinomio(hexadecimal_a_binario("4"))
-print(var)
+# Ejemplo de uso:
+A = "0xc3" # 162
+B = "0x12" # 22
+resultado = multiplicacion(A, B)
+print(resultado)  # Debería imprimir: 0x2f
