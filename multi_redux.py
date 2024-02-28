f0acabee91731bb0118447dc6e0639273517656d
Martin Romero <nahual332@outlook.com>
2024-02-28 10:20:47 -0600

    Agregue la funcion de polinomio, todo funciona bien

--- multi_redux.py
+++ multi_redux.py
@@ -18,21 +18,28 @@ def multiplicacion(hexa1, hexa2):
     resultado_hex = hex(resultado)
     return resultado_hex
 
-
-def binario_polinomio(binario):
+#Psar de hexadecimal a polinomio
+def hex_polinomio(hex):
     lista = []
-    cadena_binaria = str(binario)
+    cadena_binaria = bin(int(hex,16)) #Cambiamos de hexadecimal a binario para identifcar los 1 y guardar las posiciones
     for i in range(len(cadena_binaria)):
         if cadena_binaria[i] == "1":
             lista.append(i)
-    return lista
 
-def operacion_xor(binario1, binario2):
+    polinomio = ''
+    for i in lista[::-1]:
+        if i == 0:
+            polinomio += "1"
+        else:
+            polinomio += f"x^{i} + "
+    return polinomio
+
+def operacion_xor(hex1, hex2):
     # Ejemplo 2
-    x = 0b1010  # Representación binaria de 10
-    y = 0b1100  # Representación binaria de 12
-    result = x ^ y
-    print(bin(result))  # Output: 0b110 (Representación binaria de 6)
+    num1 = int(hex1, 16)
+    num2 = int(hex2, 16)
+    result = num1 ^ num2 #Operacion XOR entre numeros
+    return result
 
 
 # Ejemplo de uso:
@@ -40,10 +47,9 @@ num1 = input("Ingrese el primer numero hexadecimal: ")
 num2 = input("Ingrese el segundo numero hexadecimal: ")
 
 resultado_multi = multiplicacion(num1, num2)
-print(resultado_multi)
+print(f'El resulatdo de la multiplicacion es :  {resultado_multi}')
 
-
-
+print(f'El resulatdo de la multiplicacion (polinomio) :  {hex_polinomio(resultado_multi)}')
 
 
 # dividendo = binario_polinomio("110110110110")
